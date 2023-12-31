71cedb6518b79f77649c1dde49e830135655eafb
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2023-12-31 20:52:55 +0530

    modified path and headers

--- api/index.js
+++ api/index.js
@@ -10,10 +10,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   next();
 });
 
