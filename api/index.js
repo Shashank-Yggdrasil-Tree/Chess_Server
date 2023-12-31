b683e0ad46686970596a2be802e0257c5d61199f
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2023-12-31 21:13:07 +0530

    updated cors origin url

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://chess-vite-client.vercel.app/"
+    "https://chess-vite-client.vercel.app"
   );
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
@@ -39,7 +39,7 @@ const port = process.env.PORT || 4242;
 // upgrade http server to websocket server
 const io = new Server(server, {
   cors: {
-    origin: "https://chess-vite-client.vercel.app/",
+    origin: "https://chess-vite-client.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Access-Control-Allow-Origin"],
     credentials: true,
