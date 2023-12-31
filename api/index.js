82c482b49decf674259202a572eebc6acbafea7d
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2023-12-31 21:13:25 +0530

    updated cors origin url

--- api/index.js
+++ api/index.js
@@ -176,7 +176,7 @@ app.get("/api", (req, res) => {
   const path = `/api/item/${uuidV4()}`;
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item 2: <a href="${path}">${path}</a>`);
+  res.end(`Hello! Go to item 4: <a href="${path}">${path}</a>`);
 });
 
 server.listen(port, () => {
