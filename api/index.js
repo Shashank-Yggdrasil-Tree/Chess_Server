14a0143ca905f8f21030aad1a5a84fc2187fb1a2
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2023-12-31 20:53:42 +0530

    fixed bugs

--- api/index.js
+++ api/index.js
@@ -151,7 +151,7 @@ io.on("connection", (socket) => {
 });
 
 app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
+  const path = `/api/item/${uuidV4()}`;
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
